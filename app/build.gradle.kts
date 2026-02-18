b4ebc50266c9e56d361ce1d9e5a7412aec1a0d20
Hui-Hong You <hiroshi@ghostsinthelab.org>
2026-02-18 23:25:24 +0800

    Release 0.0.5
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.miyabi_hiroshi.app.meaninglesskeyboard"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.0.4"
+        versionCode = 4
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
